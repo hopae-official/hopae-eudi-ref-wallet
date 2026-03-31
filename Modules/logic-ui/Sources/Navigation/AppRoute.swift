7be39486d930984febdfd90f5da39b9a94551e07
DongGukMon <ehdrnr9346@naver.com>
2026-03-31 12:04:29 +0900

    Add intro route and IntroUiConfig to AppRoute

--- Modules/logic-ui/Sources/Navigation/AppRoute.swift
+++ Modules/logic-ui/Sources/Navigation/AppRoute.swift
@@ -18,14 +18,35 @@ import logic_business
 import logic_core
 import logic_resources
 
+public struct IntroUiConfig: UIConfigType, Equatable {
+  public let showDismissOption: Bool
+  public let nextRoute: AppRoute?
+
+  public var log: String {
+    "showDismissOption: \(showDismissOption)"
+  }
+
+  public init(showDismissOption: Bool, nextRoute: AppRoute? = nil) {
+    self.showDismissOption = showDismissOption
+    self.nextRoute = nextRoute
+  }
+
+  public static func == (lhs: IntroUiConfig, rhs: IntroUiConfig) -> Bool {
+    lhs.showDismissOption == rhs.showDismissOption
+  }
+}
+
 public enum FeatureStartupRouteModule: AppRouteModule {
 
   case startup
+  case intro(config: IntroUiConfig)
 
   public var info: (key: String, arguments: [String: String]) {
     return switch self {
     case .startup:
       (key: "Startup", arguments: [:])
+    case .intro(let config):
+      (key: "Intro", arguments: ["config": config.log])
     }
   }
 }
